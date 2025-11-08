d692e537aa679d51adf1fbd1893b067327caba6a
Mark Edwards <mark@medwards-online.co.uk>
2025-11-08 11:55:54 +0000

    Initial project setup

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from ray-tracer!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ ray_tracer/main.py
@@ -0,0 +1,6 @@
+def test() -> None:
+    print("Test!!")
+
+
+if __name__ == "__main__":
+    test()
